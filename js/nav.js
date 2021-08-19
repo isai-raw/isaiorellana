a82449d57042caae3d263176a4f3169b1926e37d
isai-raw <isai7orellana@gmailcom>
2021-08-18 19:19:03 -0600

    Seccion de contacto lista y funcional

--- js/nav.js
+++ js/nav.js
@@ -25,3 +25,24 @@ function menu(){
   console.log(`Holi`)
   var navegador = new nav
 }
+
+
+const $form = document.querySelector('#contacto')
+
+$form.addEventListener('submit', handlesubmit)
+
+async function handlesubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'aplication/json'
+    }
+  })
+  if (response.ok){
+    this.reset()
+    alert('Tu mensaje ha sido enviado, gracias por contactarme.')
+  }
+}
